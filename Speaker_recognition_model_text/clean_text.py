225040d93b7e2e984ca5ffb97cabe7a6b71e436e
Xiaoyu Zhu <48409279+zgzxy001@users.noreply.github.com>
2021-05-06 10:38:49 -0400

    Create clean_text.py

---
+++ Speaker_recognition_model_text/clean_text.py
@@ -0,0 +1,99 @@
+import re
+import nltk
+nltk.download('punkt')
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from nltk.tokenize import word_tokenize
+
+
+class Preprocessing:
+    def __init__(self, num_words, seq_len, file_path):
+        # self.data = 'data/tweets.csv'
+        #
+        self.data = file_path
+        self.num_words = num_words
+        self.seq_len = seq_len
+        self.vocabulary = None
+        self.x_tokenized = None
+        self.x_padded = None
+        self.x_raw = None
+        self.y = None
+
+        # self.x_train = None
+        # self.x_test = None
+        # self.y_train = None
+        # self.y_test = None
+
+    def load_data(self):
+        # Reads the raw csv file and split into
+        # sentences (x) and target (y)
+        df = pd.read_csv(self.data, sep=';', names=['id', 'target', 'Text'],encoding = "ISO-8859-1")
+        # df.drop(['id', 'keyword', 'location'], axis=1, inplace=True)
+        self.x_raw = df['Text'].values
+        print('self_x_raw = ', self.x_raw)
+        self.y = df['target'].values
+
+    def clean_text(self):
+        # Removes special symbols and just keep
+        # words in lower or upper form
+        self.x_raw = [str(x).lower() for x in self.x_raw]
+        self.x_raw = [re.sub(r'[^A-Za-z]+', ' ', str(x)) for x in self.x_raw]
+
+    def text_tokenization(self):
+        # Tokenizes each sentence by implementing the nltk tool
+        self.x_raw = [word_tokenize(x) for x in self.x_raw]
+
+    def build_vocabulary(self):
+        # Builds the vocabulary and keeps the "x" most frequent word
+        self.vocabulary = dict()
+        fdist = nltk.FreqDist()
+
+        for sentence in self.x_raw:
+            for word in sentence:
+                fdist[word] += 1
+
+        common_words = fdist.most_common(self.num_words)
+
+        for idx, word in enumerate(common_words):
+            self.vocabulary[word[0]] = (idx + 1)
+
+    def word_to_idx(self):
+        # By using the dictionary (vocabulary), it is transformed
+        # each token into its index based representatio
+        self.x_tokenized = list()
+
+        for sentence in self.x_raw:
+            temp_sentence = list()
+            for word in sentence:
+                if word in self.vocabulary.keys():
+                    temp_sentence.append(self.vocabulary[word])
+            self.x_tokenized.append(temp_sentence)
+
+    def padding_sentences(self):
+        # Each sentence which does not fulfill the required le
+        # it's padded with the index 0
+        pad_idx = 0
+        self.x_padded = list()
+
+        for sentence in self.x_tokenized:
+            while len(sentence) < self.seq_len:
+                sentence.insert(len(sentence), pad_idx)
+            self.x_padded.append(sentence)
+
+        self.x_padded = np.array(self.x_padded)
+        self.y = np.asarray(self.y)
+        return self.x_padded, self.y
+
+num_words = 100
+seq_len = 100
+file_path = '/content/val_full.txt'
+pr = Preprocessing(num_words, seq_len, file_path)
+pr.load_data()
+pr.clean_text()
+pr.text_tokenization()
+pr.build_vocabulary()
+pr.word_to_idx()
+x, y = pr.padding_sentences()
+print(x.shape) # (208, 100)
+print(y.shape) # (208,)
